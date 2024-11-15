dc362dd46cedae0ed6b7d2d2b036a14dbb7908bf
EKHO <lukasz.ksiazek1350@gmail.com>
2024-11-15 13:30:01 +0100

    Level generator without logic

---
+++ BITEhack/Assets/Individual/Łukasz/Room.cs
@@ -0,0 +1,91 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+public enum RoomTypes
+{
+    Square,
+    L,
+    I
+}
+
+
+public class Room : MonoBehaviour
+{
+    //1 for empty space
+    //2 for starting room
+    //3 for square
+    //4 for L
+    //5 for I
+    private int[,] squareRoomMatrix =
+    {
+        {0, 0, 0 },
+        {0, -3, 0 },
+        {0, 0, 0 },
+    };
+    private int[,] LRoomMatrix =
+    {
+        {0, -1, 0 },
+        { 0, -4, -1 },
+        { 0, 0, 0 }
+    };
+
+    private int[,] IRoomMatrix =
+    {
+        {0, 0, 0},
+        { 0, -5, -1 },
+        { 0, 0, 0 }
+    };
+    
+    
+    [SerializeField] private RoomTypes roomType;
+
+    private GameObject player;
+
+    private bool isPlayerAbove = false;
+    private bool isRoomCleared = true;
+    private int bridgeCountInThisRoom = 0;
+
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player");
+    }
+
+    void Update()
+    {
+        isPlayerAbove = false;
+        RaycastHit hit;
+        if (Physics.Raycast(player.transform.position, Vector3.down, out hit, Mathf.Infinity,
+                LayerMask.GetMask("Room")))
+        {
+            if (hit.collider.transform.IsChildOf(transform) || hit.collider.gameObject == gameObject)
+            {
+                isPlayerAbove = true;
+            }
+        }
+    }
+    
+    public int[,] GetRoomTypeMatrix()
+    {
+        if (roomType == RoomTypes.L)
+            return LRoomMatrix;
+        else if (roomType == RoomTypes.I)
+            return IRoomMatrix;
+        else
+            return squareRoomMatrix;
+    }
+    
+    public string GetRoomType()
+    {
+        if (roomType == RoomTypes.L)
+            return "L";
+        else if (roomType == RoomTypes.I)
+            return "I";
+        else
+            return "Square";
+    }
+}
